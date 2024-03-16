7ecbb1ce63973e98c4df7696d06f83fd575e1fa2
Logosph <zaklyakov2004@ya.ru>
2024-03-16 18:02:03 +0300

    Dmitry's changes

--- python/presentation/MainWindow/MainWindowPresenter.py
+++ python/presentation/MainWindow/MainWindowPresenter.py
@@ -1,10 +1,6 @@
 import python.domain.ApplicationRunUseCase as ApplicationRunUseCase
 
 class MainWindowPresenter:
-    def button_clicked(self):
-        print("Test Button")  # use case
 
     def run_button_clicked(self):
         ApplicationRunUseCase.execute()
-
-
